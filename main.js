6a2d239ced44c116c4d7063bc0ec714d98409e01
nluizsoliveira <nluizsoliveira@gmail.com>
2022-06-03 13:00:39 -0300

    feat(setup): add context model

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-for(let i = 0; i < 5; i++){
-    console.log("Bem vindos novos mebros do codelab sanca!!")
-}
+let document = {
+    type: 'file',     // 'file' or 'buffer'
+    template: html,
+    context: {
+        campus: "sanca",
+        title: "dev.learn(2019.1)",
+        name: "Nome Sobrenome",
+        nusp: "00000000",
+        startDay: "14",
+        startMonth: "Março",
+        startYear: "2019",
+        endDay: "25",
+        endMonth: "Maio",
+        endYear: "2019",
+        totalHours: "50",
+        presidentName: "Rafael Doering Soares",
+        presidentRole: "Presidente do USPCodelab Sanca",
+        professorName: "Sarita Mazzini Bruschi",
+        professorRole: "Prof. Tutora do USPCodelab Sanca"
+    },
+    path: "./certificate.pdf"    // it is not required if type is buffer
+};
