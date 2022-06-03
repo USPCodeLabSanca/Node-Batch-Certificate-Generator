6b7c08d82cb38fbaa46d174c842c86a97cd04918
nluizsoliveira <nluizsoliveira@gmail.com>
2022-06-03 13:16:09 -0300

    feat(backend): add model

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
-let document = {
-    type: 'file',     // 'file' or 'buffer'
-    template: html,
-    context: {
-        campus: "sanca",
-        title: "dev.learn(2019.1)",
-        name: "Nome Sobrenome",
-        nusp: "00000000",
-        startDay: "14",
-        startMonth: "Março",
-        startYear: "2019",
-        endDay: "25",
-        endMonth: "Maio",
-        endYear: "2019",
-        totalHours: "50",
-        presidentName: "Rafael Doering Soares",
-        presidentRole: "Presidente do USPCodelab Sanca",
-        professorName: "Sarita Mazzini Bruschi",
-        professorRole: "Prof. Tutora do USPCodelab Sanca"
+const campusInfo = {
+    "sanca": {
+        name: "USPCodeLab São Carlos",
+        president: "Gabriel Freitas Ximenes de Vasconcelos",
+        vicePresident: "Raíssa Torres Barreira",
+        professor: "Sarita Mazzini Bruschi"
     },
-    path: "./certificate.pdf"    // it is not required if type is buffer
-};
+    "leste": {
+        name: "USPCodeLab Leste",
+        president: "Não sei Leste Presidente",
+        vicePresident: "Não sei Leste Vice",
+        professor: "Não sei Leste Professor"
+    },
+    "butanta": {
+        name: "USPCodeLab Butanta",
+        president: "Não sei Butanta Presidente",
+        vicePresident: "Não sei Butanta Vice",
+        professor: "Não sei Butanta Professor"
+    },
+    "ufabc": {
+        name: "CodeLab UFABC",
+        president: "Não sei UFABC Presidente",
+        vicePresident: "Não sei UFABC Vice",
+        professor: "Não sei UFABC Professor"
+    }
+}
+
+let context = {
+    campus: campusInfo.sanca.name,
+    title: "dev.learn(2019.1)",
+    name: "Nome Sobrenome",
+    nusp: "00000000",
+    startDay: "14",
+    startMonth: "Março",
+    startYear: "2019",
+    endDay: "25",
+    endMonth: "Maio",
+    endYear: "2019",
+    totalHours: "50",
+    presidentName: campusInfo.sanca.president,
+    presidentRole: `Presidente do ${campusInfo.sanca.name}`,
+    professorName: campusInfo.sanca.professor,
+    professorRole: `Prof. Tutora do ${campusInfo.sanca.name}`
+}
+
+console.log(context)
