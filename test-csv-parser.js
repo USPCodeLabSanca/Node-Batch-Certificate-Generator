d1ae00692cb26c51c6425f1cd2fb1f7a447425b5
nluizsoliveira <nluizsoliveira@gmail.com>
2022-06-15 05:49:42 -0300

    fix(main): remove unnecessary files

--- test-csv-parser.js
+++
@@ -1,5 +0,0 @@
-const csv_parser = require('./csv-parser')
-const path = require('path')
-
-console.log(csv_parser(path.join(__dirname,'data.csv'),'utf8'))
-console.log(csv_parser(path.join(__dirname,'example.csv'),'utf8'))
