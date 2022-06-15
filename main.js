7ff2373a125a8ed481ce24acd581795b00f40d8f
nluizsoliveira <nluizsoliveira@gmail.com>
2022-06-15 05:26:50 -0300

    feat(certificate): add campusImage, presidentSign and professorSign to context

--- main.js
+++ main.js
@@ -8,25 +8,36 @@ const campusInfo = {
         name: "USPCodeLab São Carlos",
         president: "Gabriel Freitas Ximenes de Vasconcelos",
         vicePresident: "Raíssa Torres Barreira",
-        professor: "Sarita Mazzini Bruschi"
+        professor: "Sarita Mazzini Bruschi",
+        image: "https://i.imgur.com/rhS164j.png",
+        presidentSign: "https://i.imgur.com/wskh0W1.png",
+        professorSign: "https://i.imgur.com/wskh0W1.png"
     },
     "leste": {
         name: "USPCodeLab Leste",
         president: "Rodrigo Dorneles Ferreira de Souza",
         vicePresident: "Yago Silva",
-        professor: "Daniel De Angelis Cordeiro"
+        professor: "Daniel De Angelis Cordeiro",
+        image: "https://i.imgur.com/F40DizY.png", 
+        presidentSign: "https://i.imgur.com/wskh0W1.png",
+        professorSign: "https://i.imgur.com/wskh0W1.png"
     },
     "butanta": {
         name: "USPCodeLab Butanta",
         president: "Gabriel Fernandes Mota",
         vicePresident: "Luiz Carlos Costa da Silva",
-        professor: "Alfredo Goldman"
+        professor: "Alfredo Goldman",
+        image: "https://i.imgur.com/5AXmSZK.png",
+        presidentSign: "https://i.imgur.com/wskh0W1.png",
+        professorSign: "https://i.imgur.com/wskh0W1.png"
     },
     "ufabc": {
         name: "CodeLab UFABC",
         president: "Não sei UFABC Presidente",
         vicePresident: "Não sei UFABC Vice",
         professor: "Não sei UFABC Professor",
+        presidentSign: "https://i.imgur.com/wskh0W1.png",
+        professorSign: "https://i.imgur.com/wskh0W1.png"
     }, 
 }
 
@@ -38,10 +49,10 @@ for (const line of info) {
     line.campusPresident = campusInfo[line.campus].president
     line.campusVicePresident = campusInfo[line.campus].vicePresident
     line.campusProfessor = campusInfo[line.campus].professor
+    line.campusImage = campusInfo[line.campus].image
+    line.presidentSign = campusInfo[line.campus].presidentSign
+    line.professorSign = campusInfo[line.campus].professorSign
 }
-
-
-console.log(info)
 
 let templatePath = path.join(__dirname, 'template.html');
 let certificatePath = path.join(__dirname, 'certificate.pdf');
